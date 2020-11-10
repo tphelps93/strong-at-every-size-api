6494a184586644b92f5f8d5d0b5df05d4c48dfb1
Taylor Phelps <tphelps1993@gmail.com>
2020-11-10 11:04:11 -0500

    init deploy read

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const app = require('./app');
+const express = require('express');
 const { PORT } = require('./config');
+
+app.get('/api/*', (req, res) => {
+  res.json({ ok: true });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+module.exports = { app };
